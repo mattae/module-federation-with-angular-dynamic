a71219f339ffd4b3e3f8536ebfcc1fa4a3c842d1
mattae <matthew.edor@gmail.com>
2021-01-12 20:38:18 +0000

    Initial commit

---
+++ projects/mfe1/webpack.config.js
@@ -0,0 +1,26 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+  output: {
+    uniqueName: "mfe1"
+  },
+  optimization: {
+    // Only needed to bypass a temporary bug
+    runtimeChunk: false
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "mfe1",
+      filename: "remoteEntry.js",
+      exposes: {
+        './Component': './projects/mfe1/src/app/app.component.ts',
+        './Module': './projects/mfe1/src/app/flights/flights.module.ts'
+      },
+      shared: {
+        "@angular/core": { singleton: true, strictVersion: true }, 
+        "@angular/common": { singleton: true, strictVersion: true }, 
+        "@angular/router": { singleton: true, strictVersion: true }
+      }
+    }),
+  ],
+};
---
+++ projects/mfe2/webpack.config.js
@@ -0,0 +1,26 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+  output: {
+    uniqueName: "mfe2"
+  },
+  optimization: {
+    // Only needed to bypass a temporary bug
+    runtimeChunk: false
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "mfe2",
+      filename: "remoteEntry.js",
+      exposes: {
+        './Component': './projects/mfe2/src/app/app.component.ts',
+        './Module': './projects/mfe2/src/app/bookings/bookings.module.ts'
+      },
+      shared: {
+        "@angular/core": { singleton: true, strictVersion: true }, 
+        "@angular/common": { singleton: true, strictVersion: true }, 
+        "@angular/router": { singleton: true, strictVersion: true }
+      }
+    }),
+  ],
+};
